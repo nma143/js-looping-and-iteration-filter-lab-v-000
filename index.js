89c713cf8fcf207c6e1604167ad3bf65d628d958
Nikki A <nikki.apostolakis@gmail.com>
2019-04-07 16:17:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // Code your solution in this file
-function findMatching()
+function findMatching(drivers, name)
 {
-
+  const newDrivers = [];
+  for (const driver of drivers)
+  {
+    if driver == name
+    {
+      newDrivers.push(driver)
+    }
+  }
+  return newDrivers;
 }
 function fuzzyMatch()
 {
